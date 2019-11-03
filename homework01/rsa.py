51016b51063d77043ea4d0096bc77d58d8b7ccf9
abinochu <56220825+abinochu@users.noreply.github.com>
2019-11-04 01:39:32 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -2,6 +2,16 @@ import random
 
 
 def is_prime(num: int):
+    """
+    Tests to see if a number is prime.
+    
+    >>> is_prime(2)
+    True
+    >>> is_prime(11)
+    True
+    >>> is_prime(8)
+    False
+    """
     counter = 0
     for i in range(1, num + 1):
         if num % i == 0:
@@ -11,21 +21,12 @@ def is_prime(num: int):
     else:
         return('False')
 
-    """
-    Tests to see if a number is prime.
-    >>> is_prime(2)
-    True
-    >>> is_prime(11)
-    True
-    >>> is_prime(8)
-    False
-    """
-pass
+   
 
 
 def gcd(a, b):
     """
-     Euclid's algorithm for determining the greatest common divisor.
+    Euclid's algorithm for determining the greatest common divisor.
 
     >>> gcd(12, 15)
     3
@@ -39,7 +40,6 @@ def gcd(a, b):
         else:
             b = b - a
     return (a)
-pass
 
 
 def multiplicative_inverse(e, phi):
@@ -54,7 +54,6 @@ def multiplicative_inverse(e, phi):
     while d * e % phi != 1:
         d += 1
     return (d)
-pass
 
 
 def generate_keypair(p, q):
@@ -63,59 +62,59 @@ def generate_keypair(p, q):
     elif p == q:
         raise ValueError('p and q cannot be equal')
 
-# n = pq
-n = p*q
+    # n = pq
+    n = p*q
 
-# phi = (p-1)(q-1)
-phi = (p-1)*(q-1)
+    # phi = (p-1)(q-1)
+    phi = (p-1)*(q-1)
 
-# Choose an integer e such that e and phi(n) are coprime
+    # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
 
-# Use Euclid's Algorithm to verify that e and phi(n) are comprime
+    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
     g = gcd(e, phi)
-while g != 1:
-    e = random.randrange(1, phi)
-    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
 
-# Use Extended Euclid's Algorithm to generate the private key
+    # Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
 
-# Return public and private keypair
-# Public key is (e, n) and private key is (d, n)
-return ((e, n), (d, n))
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n))
 
 
 def encrypt(pk, plaintext):
-# Unpack the key into it's components
+    # Unpack the key into it's components
     key, n = pk
-# Convert each letter in the plaintext to numbers based on
-# the character using a^b mod m
+    # Convert each letter in the plaintext to numbers based on
+    # the character using a^b mod m
     cipher = [(ord(char) ** key) % n for char in plaintext]
-# Return the array of bytes
-return cipher
+    # Return the array of bytes
+    return cipher
 
 
 def decrypt(pk, ciphertext):
-# Unpack the key into its components
+    # Unpack the key into its components
     key, n = pk
-# Generate the plaintext based on the ciphertext and key using a^b mod m
+	# Generate the plaintext based on the ciphertext and key using a^b mod m
     plain = [chr((char ** key) % n) for char in ciphertext]
-# Return the array of bytes as a string
-return ''.join(plain)
+    # Return the array of bytes as a string
+    return ''.join(plain)
 
 
 if __name__ == '__main__':
-print("RSA Encrypter/ Decrypter")
+    print("RSA Encrypter/ Decrypter")
     p = int(input("Enter a prime number (17, 19, 23, etc): "))
     q = int(input("Enter another prime number (Not one you entered above): "))
-print("Generating your public/private keypairs now . . .")
+    print("Generating your public/private keypairs now . . .")
     public, private = generate_keypair(p, q)
-print("Your public key is ", public, " and your private key is ", private)
+    print("Your public key is ", public, " and your private key is ", private)
     message = input("Enter a message to encrypt with your private key: ")
     encrypted_msg = encrypt(private, message)
-print("Your encrypted message is: ")
-print(''.join(map(lambda x: str(x), encrypted_msg)))
-print("Decrypting message with public key ", public, " . . .")
-print("Your message is:")
-print(decrypt(public, encrypted_msg))
+    print("Your encrypted message is: ")
+    print(''.join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public, " . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
