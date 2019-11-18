c307b6c2959b8716b53c47fe16a15b0d9c6e5c8d
abinochu <56220825+abinochu@users.noreply.github.com>
2019-11-18 23:34:38 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -16,7 +16,7 @@ def is_prime(num: int):
     for i in range(1, num + 1):
         if num % i == 0:
             counter += 1
-    if counter == 2:
+    if counter >= 2:
         return('True')
     else:
         return('False')
